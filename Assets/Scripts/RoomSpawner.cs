38a4e57014556a6345f7e714c9bab87ae5dc6bcf
carribasa <carribasa93@gmail.com>
2024-02-29 20:36:23 +0100

    Creadas salas con spawner y prefabs

--- Assets/Scripts/RoomSpawner.cs
+++ Assets/Scripts/RoomSpawner.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class RoomSpawner : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public int openSide;
+
+    private RoomTemplates templates;
+    private int rand;
+    private bool spawned = false;
+
     void Start()
     {
-        
+        templates = GameObject.FindGameObjectWithTag("Rooms").GetComponent<RoomTemplates>();
+        Invoke("Spawn", 0.1f);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Spawn()
     {
-        
+        if (openSide == 1)
+        {
+            rand = Random.Range(0, templates.bottomRooms.Length);
+            Instantiate(templates.bottomRooms[rand], transform.position, templates.bottomRooms[rand].transform.rotation);
+        }
+        else if (openSide == 2)
+        {
+
+        }
+        else if (openSide == 3)
+        {
+
+        }
+        else if (openSide == 4)
+        {
+
+        }
     }
 }
